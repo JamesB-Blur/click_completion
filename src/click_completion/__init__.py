2675dea54c0dcb37dae51be0355fa2145c3b4699
james-berkheimer <james.berkheimer@gmail.com>
2023-07-27 14:16:37 -0700

    first commit

---
+++ src/click_completion/__init__.py
@@ -0,0 +1,3 @@
+# import os
+
+# os.environ["_COMPLETION_COMPLETE"] = "bash_source completion"
---
+++ src/click_completion/__main__.py
@@ -0,0 +1,6 @@
+# from click_completion.completion import main
+# from completion import main
+from click_completion.completion import main
+
+if __name__ == "__main__":
+    main()
